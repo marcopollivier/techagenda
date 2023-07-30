2a6cf334e9b5d9e4044b66edfd054bab087b98c7
Marco Ollivier <marcopollivier@gmail.com>
2023-07-30 00:51:44 -0300

    Start a new event list component

---
+++ src/components/EventList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>Lista de eventos</h2>
+        <ul>
+            <li f-for="event  in events" :key="event.title">
+
+            </li>
+        </ul>
+    </div>
+</template>
