4fbc7e03f3014e0f3d18a919a5696be8d9d17c33
Everest <caioeverest.b@gmail.com>
2024-01-16 01:30:13 -0300

    rename http port variable to match heroko spec

--- lib/config/config.go
+++ lib/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type Config struct {
-	HTTPPort    int       `env:"HTTP_PORT" envDefault:"8000"`
+	HTTPPort    int       `env:"PORT" envDefault:"8000"`
 	Environment string    `env:"ENVIRONMENT" envDefault:"unknown"`
 	LogLevel    string    `env:"LOG_LEVEL" envDefault:"debug"`
 	LogFormat   string    `env:"LOG_FORMAT" envDefault:"text"`
