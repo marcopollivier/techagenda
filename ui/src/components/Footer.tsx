d55c35de0e190df538dfdae9e79891a26259374e
Everest <caioeverest.b@gmail.com>
2024-01-21 00:25:10 -0300

    refine header and footer components

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import TechAgendaLogoFooter from "../../public/logo.svg";
 
 export default function Footer() {
     return (
-        <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="relative flex h-32 items-center justify-between">
+        <div className="bg-white">
+            <div className="mx-auto">
+                <div className="relative h-32 flex justify-between">
                     <div className="flex flex-shrink-0 items-center">
                         <img
                             className="h-10 w-auto"
@@ -12,25 +12,21 @@ export default function Footer() {
                             alt="TechAgenda"
                         />
                     </div>
-                    <div className="mx-auto max-w-2xl lg:mx-0">
-                        <p className="hover:bg-gray-100 hover:text-blue-500 bg-blue-500 text-white rounded-full px-3 py-1 text-sm font-medium text-lg">Voltar ao topo</p>
+                    <div className="grid w-40 content-center">
+                        <p className="hover:bg-gray-100 hover:text-blue-500 bg-blue-500 text-white rounded-full px-3 py-1 text-sm font-medium text-lg text-center">Voltar ao topo</p>
                     </div>
                 </div>
-                <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"></div>
+                <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-4 lg:mx-0 lg:max-w-none lg:grid-cols-3"></div>
                 <div className="flex items-center justify-between">
-                    <p className="flex">
-                        © 2024 Tech Agenda
-                    </p>
+                    <p className="flex">© 2024 Tech Agenda</p>
                     <div className="flex items-center justify-between text-slate-700 antialiased font-semibold">
-                        <p className="hover:bg-gray-200 hover:text-black rounded-full px-3 py-1 text-sm">Github</p>
+                        <p className="hover:bg-blue-500 hover:text-white rounded-full px-3 py-1 text-sm">Github</p>
                         <div className="px-4">
                             <div className="rounded border-2 border-slate-700"></div>
                         </div>
-                        <p className="hover:bg-gray-200 hover:text-black rounded-full px-3 py-1 text-sm">Termos</p>
+                        <p className="hover:bg-blue-500 hover:text-white rounded-full px-3 py-1 text-sm">Termos</p>
                     </div>
-                    <p className="flex">
-                        Construído com tecnologia brasileira
-                    </p>
+                    <p className="flex self-en">Construído com tecnologia brasileira</p>
                 </div>
             </div>
         </div>
--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -18,11 +18,11 @@ function classNames(...classes) {
 
 export default function Header() {
     return (
-        <Disclosure as="nav" className="pb-6">
+        <Disclosure as="nav" className="pt-8 pb-6">
             {({ open }) => (
                 <>
                     <div className="">
-                        <div className="relative flex h-16 items-center justify-between">
+                        <div className="relative flex h-10 items-center justify-between">
                             {/* Logo */}
                             <div className="flex flex-shrink-0 items-center">
                                 <img
@@ -92,7 +92,7 @@ export default function Header() {
                             </div>
                         </div>
                         {/* Main menu options */}
-                        <div className="relative flex h-26 pt-28 items-center justify-between">
+                        <div className="relative flex h-20 pt-24 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
                                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
