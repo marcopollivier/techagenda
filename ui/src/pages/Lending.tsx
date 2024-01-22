fedb9475505d5831656bb07c2611c96997462fae
Everest <caioeverest.b@gmail.com>
2024-01-22 01:33:00 -0300

    built event list component

--- ui/src/pages/Lending.tsx
+++ ui/src/pages/Lending.tsx
@@ -1,15 +1,20 @@
 //import { useState } from "react";
 //import { IndexRouteProps } from "../generated";
 import Header from "../components/Header";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
+import MapBanner from "../components/MapBanner";
+import AdBanner from "../components/AdBanner";
+import EventList from "../components/EventList";
 
 function Lending() {
     //const [count, setCount] = useState(initialCount);
 
     return (
-        <div className="mx-auto max-w-8xl px-20 sm:px-30 lg:px-20">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <Header />
-            <a>test</a>
+            <EventList />
+            <MapBanner />
+            <AdBanner />
             <Footer />
         </div>
     );
